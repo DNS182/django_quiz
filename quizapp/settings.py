59c7eb3aa1284ed12b3f6b553f06cc94dc60c7c1
Dinesh <182dns@gmail.com>
2022-04-29 09:55:56 +0545

    Pushed for deployment

--- quizapp/settings.py
+++ quizapp/settings.py
@@ -22,12 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!98x5#(u2ua6%)t&4^456)q@q0q-d(xiaq+l8$6t5jv^i7=d*d'
+SECRET_KEY =  os.environ.get("SECRET_KEY")
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['quiz182.herokuapp.com','127.0.0.1:8000']
 
 
 # Application definition
--- quizapp/urls.py
+++ quizapp/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path , include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('hero/', admin.site.urls),
     path('' ,include('quizz.urls')),
     path('accounts/' , include('accounts.urls'))    
 ]
