144e0630ee747dbb563d198abe3b8e946a8be184
Dinesh <182dns@gmail.com>
2022-04-29 10:55:26 +0545

    collect static

--- quizapp/settings.py
+++ quizapp/settings.py
@@ -24,10 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY =  os.environ.get("SECRET_KEY")
 
-
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['quiz182.herokuapp.com','127.0.0.1:8000']
 
